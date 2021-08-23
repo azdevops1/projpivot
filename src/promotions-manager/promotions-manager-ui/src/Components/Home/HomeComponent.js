6573c6f9cb69a997e7f56ad8cfb25029505a155b
Alex Irizarry <azdevops1@outlook.com>
2021-08-23 10:35:51 -0500

    change bkgr color

--- src/promotions-manager/promotions-manager-ui/src/Components/Home/HomeComponent.js
+++ src/promotions-manager/promotions-manager-ui/src/Components/Home/HomeComponent.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {Button, Form, FormControl, Grid, Row, Col} from 'react-bootstrap';
 
 const signUpSection = {
-    backgroundColor: '#e31212',
+    backgroundColor: '#ff2812',
     margin: '0 -26.8% 0 -26.8%',
 }
 const signUpDiv = {
