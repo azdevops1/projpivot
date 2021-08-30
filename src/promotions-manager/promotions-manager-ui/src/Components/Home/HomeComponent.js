c32ee8c8f1899bc2b4e14775fc1464e79965dfd8
Alex Irizarry <azdevops1@outlook.com>
2021-08-30 12:27:23 -0500

    change background signup color

--- src/promotions-manager/promotions-manager-ui/src/Components/Home/HomeComponent.js
+++ src/promotions-manager/promotions-manager-ui/src/Components/Home/HomeComponent.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {Button, Form, FormControl, Grid, Row, Col} from 'react-bootstrap';
 
 const signUpSection = {
-    backgroundColor: '#ff2812',
+    backgroundColor: '#C500BD',
     margin: '0 -26.8% 0 -26.8%',
 }
 const signUpDiv = {
